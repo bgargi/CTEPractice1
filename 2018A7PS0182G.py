d622b517bee3aa99d88ac99b7fa35c2342ac8061
antailbaxt3r <arjunbajpaidps@gmail.com>
2018-09-12 12:30:17 +0530

    2018A7PS0182G

---
+++ 2018A7PS0182G.py
@@ -0,0 +1,4 @@
+id = "2018A7PS0182G"
+sum = int(id[0])+int(id[1])+int(id[2])+int(id[3])+int(id[5])+int(id[8])+int(id[9])+int(id[10])+int(id[11])
+answer = str(sum)
+print("Your ID is "+id +" and the sum of all digits is",sum)
